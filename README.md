9353d5786a99618c05bfcb7350627eb751938612
Kristopher Overholt <kris@koverholt.com>
2020-09-04 23:53:43 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # austin-fire-incidents
-Source for https://algorithmia.com/algorithms/koverholt/AustinFireIncidents
+
+Interactive Map of Austin Fire Incidents from 2013 to 2018
+
+![Austin Fire Incidents](https://github.com/koverholt/austin-fire-incidents/blob/master/images/austin-fire-incidents.png?raw=true)
+
+Data from https://data.austintexas.gov
+
+Algorithm at https://algorithmia.com/algorithms/koverholt/AustinFireIncidents
--- src/AustinFireIncidents.py
+++ src/AustinFireIncidents.py
@@ -1,6 +1,34 @@
 import Algorithmia
+import numpy as np
+import pandas as pd
 
-# API calls will begin at the apply() method, with the request body passed as 'input'
-# For more details, see algorithmia.com/developers/algorithm-development/languages
+client = Algorithmia.client("simSSc1M+5bDh1CtJ/qnBs0SBba1")
+
+df = pd.concat(map(pd.read_csv, [
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2013_January_Thru_December.csv").getFile(),
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2014_January_Thru_December.csv").getFile(),
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2015_January_Thru_December.csv").getFile(),
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2016_January_Thru_December.csv").getFile(),
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2017_January_-_December.csv").getFile(),
+    client.file("data://koverholt/AustinFireIncidents/AFD_Fire_Incidents_2018_January_-_December.csv").getFile(),
+]))
+
+df[["Latitude", "Longitude"]] = df["Location 1"].str.split(", ", expand=True)
+
+df["Latitude"] = df["Latitude"].str.strip("()")
+df["Longitude"] = df["Longitude"].str.strip("()")
+
+df["Latitude"] = df["Latitude"].map(lambda x: "{:.0f}".format(float(x)))
+df["Longitude"] = df["Longitude"].map(lambda x: "{:.0f}".format(float(x)))
+
+df["Latitude"] = df["Latitude"].replace("nan", np.nan)
+df["Longitude"] = df["Longitude"].replace("nan", np.nan)
+
+df = df.dropna(subset=["Latitude", "Longitude"])
+
+df["Latitude"] = df["Latitude"].astype(int) / 1000000
+df["Longitude"] = df["Longitude"].astype(int) / -1000000
+
+
 def apply(input):
-    return "hello {}".format(input)
+    return df.to_json(orient="records")
