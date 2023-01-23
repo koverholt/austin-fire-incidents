3f4135b1647269a3edc15d6360b2272b73d59cc5
Kristopher Overholt <koverholt@gmail.com>
2023-01-23 14:59:22 -0600

    Add Angular version of site

---
+++ austin-fires/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'austin-fires';
+
+  data: any;
+  row: any;
+  rows: any;
+  key: any;
+
+  constructor() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://us-central1-koverholt-apps-304316.cloudfunctions.net/austin-fire-incidents");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+
+    xhr.onload = function () {
+      var obj = JSON.parse(this.response);
+
+      function unpack(rows: any, key: any) {
+        return rows.map(function(row: any) { return row[key]; });
+      }
+
+      var classArray = unpack(obj, 'Problem');
+      var classes = [...new Set(classArray)];
+
+      var data = classes.map(function(classes) {
+        var rowsFiltered = obj.filter(function(row: any) {
+            return (row.Problem === classes);
+        });
+        return {
+            type: 'scattermapbox',
+            name: classes,
+            lat: unpack(rowsFiltered, 'Latitude'),
+            lon: unpack(rowsFiltered, 'Longitude'),
+        };
+      });
+
+      var layout = {
+        autosize: true,
+        hovermode: 'closest',
+        showlegend: true,
+        margin: {
+          r: 0,
+          t: 0,
+          b: 0,
+          l: 0,
+          pad: 0
+        },
+        mapbox: {
+          style: "carto-positron",
+          bearing: 0,
+          center: {
+            lat: 30.30088,
+            lon: -97.7597,
+          },
+          pitch: 0,
+          zoom: 9
+        },
+      }
+
+      Plotly.newPlot('map', data, layout)
+
+    };
+
+  }
+}
